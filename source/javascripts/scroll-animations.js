a7b090de2ffe0cca9ef9bdbcc7ba8efbe7980c65
Michelle Venetucci Harvey <michellevenetucci@Michelles-MacBook-Pro.local>
2014-10-29 14:43:16 -0700

    refactor scroll animations

--- source/javascripts/scroll-animations.js
+++ source/javascripts/scroll-animations.js
@@ -1,40 +1,18 @@
 $(window).scroll(function (event) {
   var y = $(this).scrollTop();
 
-  function animationClass() {
-    $(this).addClass('animate-left');
+  function animateBoxes(parent) {
+   var firstSelector = parent + ' .box-left';
+   var secondSelector = parent + ' .box-right';
+   $(firstSelector).addClass('animate')
+   $(secondSelector).addClass('animate')
   }
 
   if (y >= 300) {
-    $('.second-graphic .box-left').addClass('animate');
-    $('.second-graphic .box-right').addClass('animate');
+   animateBoxes('.second-graphic')
   }
 
   if (y >= 500) {
-    $('.third-graphic .box-left').addClass('animate');
-    $('.third-graphic .box-right').addClass('animate');
-  }
-
-  if (y >= 700) {
-    $('.fourth-graphic .box-left').addClass('animate');
-    $('.fourth-graphic .box-right').addClass('animate');
+   animateBoxes('.third-graphic')
   }
 });
-
-function animateBoxes(parent) {
- var firstSelector = parent + ' .box-left';
- var secondSelector = parent + ' .box-right';
- $(firstSelector).addClass('animate')
- setTimeout(function(){
-   $(secondSelector).addClass('animate')
- }, 400)
-}
-
-
-if (y >= 300) {
- animateBoxes('.second-graphic')
-}
-
-if (y >= 500) {
- animateBoxes('.third-graphic')
-}
