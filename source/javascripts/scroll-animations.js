e1901294a94277ad1150661f2f0df3249d5c5785
Michelle Venetucci Harvey <michellevenetucci@Michelles-MacBook-Pro.local>
2014-10-29 11:51:12 -0700

    target children wip

--- source/javascripts/scroll-animations.js
+++ source/javascripts/scroll-animations.js
@@ -1,18 +1,20 @@
 $(window).scroll(function (event) {
   var y = $(this).scrollTop();
+  var boxes = $( event.target ).children();
+  var animation = boxes.addClass( "animate-left" );
 
   if (y >= 300) {
-    $('.second-graphic').addClass('animate');
+    $('.second-graphic img').scroll(animation);
     console.log(y);
   }
 
   if (y >= 500) {
-    $('.third-graphic').addClass('animate');
+    $('.third-graphic').addClass('animate-left');
     console.log(y);
   }
 
   if (y >= 700) {
-    $('.fourth-graphic').addClass('animate');
+    $('.fourth-graphic').addClass('animate-left');
     console.log(y);
   }
 });
